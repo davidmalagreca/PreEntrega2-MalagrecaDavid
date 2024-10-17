30b906df3b0fb0aa96346caa161ffa4ee84767e1
David Malagreca <malagrecadavid@gmail.com>
2024-10-17 18:49:13 -0300

    agrego boton finalizar compra

--- index.js
+++ index.js
@@ -52,18 +52,18 @@ while (true) {
 
 console.log(`El total de tu compra es: ${total.toLocaleString('es-ES')} pesos.`);
 
-//Agrego Evento con click para consola.////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const añadirCarrito = document.getElementById("añadirCarrito");
 const descontarCarrito = document.getElementById("descontarCarrito");
 let totalRemera = 0;
 
-añadirCarrito.addEventListener('click', function () {
+añadirCarrito.addEventListener(`click`, function () {
     totalRemera++; 
     console.log(`Añadiste al carrito una remera $23.500. Total en el carrito: ${totalRemera}`);
     alert("Agregaste una remera al carrito");
 });
 
-descontarCarrito.addEventListener('click', function () {
+descontarCarrito.addEventListener(`click`, function () {
     if (totalRemera > 0) {
         totalRemera--; 
         console.log(`Quitaste una remera. Total en el carrito: ${totalRemera}`);
@@ -73,7 +73,7 @@ descontarCarrito.addEventListener('click', function () {
     }
 });
 
-/////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const añadirCarrito2 = document.getElementById("añadirCarrito2");
 const descontarCarrito2 = document.getElementById("descontarCarrito2");
@@ -82,33 +82,46 @@ let totalBuzo = 0;
 añadirCarrito2.addEventListener(`click`, function () {
     totalBuzo++;
     console.log(`Añadiste al carrito un buzo $45.000. Total en el carrito: ${totalBuzo}`);
-    alert("Agregaste un buzo al carrito")
-})
-descontarCarrito2.addEventListener('click', function () {
+    alert("Agregaste un buzo al carrito");
+});
+
+descontarCarrito2.addEventListener(`click`, function () {
     if (totalBuzo > 0) {
         totalBuzo--; 
-        console.log(`Quitaste una buzo. Total en el carrito: ${totalBuzo}`);
-        alert("Quitaste una buzo del carrito");
+        console.log(`Quitaste un buzo. Total en el carrito: ${totalBuzo}`);
+        alert("Quitaste un buzo del carrito");
     } else {
         alert("No hay artículos en el carrito para quitar.");
     }
 });
-////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const añadirCarrito3 = document.getElementById("añadirCarrito3");
 const descontarCarrito3 = document.getElementById("descontarCarrito3");
 let totalJean = 0;
 
 añadirCarrito3.addEventListener(`click`, function () {
-    console.log(`Añadiste al carrito un jean $75.000`)
-    alert("Agregaste un jean al carrito")
-})
-descontarCarrito3.addEventListener('click', function () {
+    totalJean++; 
+    console.log(`Añadiste al carrito un jean $75.000. Total en el carrito: ${totalJean}`);
+    alert("Agregaste un jean al carrito");
+});
+
+descontarCarrito3.addEventListener(`click`, function () {
     if (totalJean > 0) {
         totalJean--; 
-        console.log(`Quitaste una jean. Total en el carrito: ${totalJean}`);
-        alert("Quitaste una jean del carrito");
+        console.log(`Quitaste un jean. Total en el carrito: ${totalJean}`);
+        alert("Quitaste un jean del carrito");
     } else {
         alert("No hay artículos en el carrito para quitar.");
     }
 });
+
+// Finalizar Compra
+const finalizarCompra = document.getElementById("finalizarCompra");
+finalizarCompra.addEventListener(`click`, function () {
+    const totalCompra = totalJean * 75000 + totalBuzo * 45000 + totalRemera * 23500; 
+    console.log(`El total de la compra es: $${totalCompra}`);
+    alert(`El total de la compra es: $${totalCompra}`);
+});
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
